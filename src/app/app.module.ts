3b870f754acdac87690e74d289e94af0f81d9fda
LuytBui <luytbq43@gmail.com>
2022-04-27 15:23:02 +0700

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterAndLoginComponent } from './register-and-login/register-and-login.component';
+import { RegisterComponent } from './register-and-login/register/register.component';
+import { LoginComponent } from './register-and-login/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterAndLoginComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
